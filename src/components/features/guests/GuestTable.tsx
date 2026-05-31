65d3cb4f8963c6fb9d3c3df224081a0158188c9b
jscottvogel <j_scott_vogel@yahoo.com>
2026-05-31 07:15:15 -0500

    impot bug

--- src/components/features/guests/GuestTable.tsx
+++ src/components/features/guests/GuestTable.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Plus, Trash2, Edit2, Check, X, Search, Filter, UserPlus } from 'lucide-react';
 import type { Schema } from '../../../../amplify/data/resource';
 
